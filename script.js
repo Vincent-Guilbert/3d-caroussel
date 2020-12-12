33941de2bfb3089413d9d0a80646a7f4615cdc1d
Vincent-Guilbert <vincentguilbert13@protonmail.com>
2020-12-12 23:09:26 +0100

    add two ways controll and style

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let deg = 60;
+let deg = 0;
 
-document.querySelector('.btn').addEventListener('click', () => {
+document.querySelector('.right-control').addEventListener('click', () => rotate(deg -= 60))
+document.querySelector('.left-control').addEventListener('click', () => rotate(deg += 60))
+
+function rotate(increment) {
     let caroussel = document.querySelector('.all-plans');
-    caroussel.style.transform = `rotateY(${deg}deg)`
-    deg += 60;
-})
+    caroussel.style.transform = `rotateY(${deg}deg)`;
+    increment;
+}
